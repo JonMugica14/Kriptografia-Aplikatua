5ea0e233a73d363b6f35c287e94230ef56728d4d
964553 <964553@U112054.gi>
2024-10-02 10:24:47 +0200

    [200~search egina?

--- 4.Praktika/decrypt.c
+++ 4.Praktika/decrypt.c
@@ -4,22 +4,23 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <time.h>
+#include <math.h>
 #include "./tiny_aes/aes.h"
 
 #define BLOCK_SIZE 16
 #define AES_KEY_LENGTH 32
-#define KEY_LENGTH 32 
+#define KEY_LENGTH 32
 #define RANGE 256
 
 uint8_t aux[BLOCK_SIZE];
 
-uint8_t iv[] = { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f };
+uint8_t iv[] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f};
 
 void print_hex(uint8_t *buf, uint32_t c)
 {
     uint32_t i;
 
-    for(i = 0; i < c; i++)
+    for (i = 0; i < c; i++)
     {
         printf("%.2x", buf[i]);
     }
@@ -29,12 +30,12 @@ void print_hex(uint8_t *buf, uint32_t c)
 uint32_t hexdigit_value(uint8_t c)
 {
     int nibble = -1;
-    if(('0' <= c) && (c <= '9')) 
-        nibble = c-'0';
-    if(('a' <= c) && (c <= 'f'))
-        nibble = c-'a' + 10;
-    if(('A' <= c) && (c <= 'F'))
-        nibble = c-'A' + 10;
+    if (('0' <= c) && (c <= '9'))
+        nibble = c - '0';
+    if (('a' <= c) && (c <= 'f'))
+        nibble = c - 'a' + 10;
+    if (('A' <= c) && (c <= 'F'))
+        nibble = c - 'A' + 10;
     return nibble;
 }
 
@@ -44,85 +45,119 @@ void parse(uint32_t length, uint8_t *in, uint8_t *out)
     uint8_t nibble, c;
     uint32_t len = strlen(in);
 
-    if(length >(len/2))
-        length = (len/2);
+    if (length > (len / 2))
+        length = (len / 2);
     memset(out, 0, length);
-    for(i = 0;i < length * 2;i++)
+    for (i = 0; i < length * 2; i++)
     {
         shift = 4 - 4 * (i & 1);
-        idx = i;//len-1-i;
+        idx = i; // len-1-i;
         c = in[idx];
         nibble = hexdigit_value(c);
-        out[i/2] |= nibble << shift;
+        out[i / 2] |= nibble << shift;
     }
 }
 
-uint32_t parse_mask(uint8_t *in, int64_t **key_mask){
-    
-    uint64_t i = 0;                                                                                                                             
-    uint32_t n_masks = 0;                                                                                                                      
-    char *end_ptr;                                                                                                                         
-    uint8_t *in_aux = strdup(in);                                                                                                     
-    uint8_t *pt = strtok (in_aux,"_");                                                                                                    
-    while (pt != NULL) 
-    {                                                                                                                   
-        if(strtol(pt, &end_ptr, 10) == -1)
+uint32_t parse_mask(uint8_t *in, int64_t **key_mask)
+{
+
+    uint64_t i = 0;
+    uint32_t n_masks = 0;
+    char *end_ptr;
+    uint8_t *in_aux = strdup(in);
+    uint8_t *pt = strtok(in_aux, "_");
+    while (pt != NULL)
+    {
+        if (strtol(pt, &end_ptr, 10) == -1)
         {
             n_masks = 0;
-            return(n_masks);
+            return (n_masks);
         }
-        n_masks++;                                                                                                                        
-        pt = strtok (NULL, "_");                                                                                                           
-    }                                                                                                                                      
-    *key_mask = malloc(n_masks * sizeof(int64_t));                                                                                               
-                                                                                                                                           
-    pt = strtok (in, "_");                                                                                                              
-    while (pt != NULL) 
-    {                                                                                                                   
-        (*key_mask)[i++] = strtol(pt, &end_ptr, 10);                                                                                            
-        pt = strtok (NULL, "_");                                                                                                           
-    }                                                                                                                                      
-    return(n_masks);                                                                                                                      
+        n_masks++;
+        pt = strtok(NULL, "_");
+    }
+    *key_mask = malloc(n_masks * sizeof(int64_t));
+
+    pt = strtok(in, "_");
+    while (pt != NULL)
+    {
+        (*key_mask)[i++] = strtol(pt, &end_ptr, 10);
+        pt = strtok(NULL, "_");
+    }
+    return (n_masks);
 }
 
 void search(int64_t n_key_mask, int64_t *key_mask, int64_t n_plaintext_mask, int64_t *plaintext_mask, uint8_t *key, uint8_t *plain_text, uint8_t *cypher_text)
-{	
-	//RELLENA EL CODIGO
+{
+    int i = 0, j = 0, aurkitua = 0;
+    int a = 0, b = 0, c = 0, d = 0;
+
+    double aukera = pow(2, 32);
+
+    struct AES_ctx ctx;
+
+    uint8_t cypher_text_prima[BLOCK_SIZE];
+
+    while (i < aukera && aurkitua == 0) {
+
+        if (a == 255) {
+            a = 0;
+            if (b == 255) {
+                b = 0;
+                if (c == 255) {
+                    c = 0;
+                    d++;
+                } else c++;
+            } else b++;
+        } else a++;
+
+        key[28] = a;
+        key[29] = b;
+        key[30] = c;
+        key[31] = d; 
+
+        memcpy(cypher_text_prima, cypher_text, BLOCK_SIZE);   
+
+        AES_init_ctx_iv(&ctx, key, iv);
+        AES_CBC_decrypt_buffer(&ctx, cypher_text_prima, BLOCK_SIZE);
+
+        if (0 == memcmp(plain_text, cypher_text_prima, BLOCK_SIZE)) printf("SUCCESS!\n");  
+    }
 }
 
 int main(int argc, char *argv[])
 {
+    int64_t n_key_mask;
+    int64_t n_plaintext_mask;
+    uint8_t key[AES_KEY_LENGTH];
+    uint8_t plain_text[BLOCK_SIZE];
+    uint8_t cypher_text[BLOCK_SIZE];
+    int64_t *key_mask;
+    int64_t *plaintext_mask;
+    uint64_t n_threads, n_threads_sys;
+    char *end_ptr;
 
-	int64_t n_key_mask;
-	int64_t n_plaintext_mask;
-	uint8_t key[AES_KEY_LENGTH];
-	uint8_t plain_text[BLOCK_SIZE];
-	uint8_t cypher_text[BLOCK_SIZE];
-	int64_t *key_mask;
-	int64_t *plaintext_mask;
-	uint64_t n_threads, n_threads_sys;
-	char *end_ptr;
-	
-	if(argc != 6 && argc != 7)
-	{
-		fprintf(stderr, "Usage: %s key key_mask plaintext plaintext_mask cyphertext\n", argv[0]);
-		return(0);
-	}
-	if(argc == 6)
-		n_threads = 1;
-	else
-	{
-		n_threads_sys = sysconf(_SC_NPROCESSORS_ONLN);
-		n_threads = strtol(argv[6], &end_ptr, 10);
-		if(n_threads > n_threads_sys)
-	    		n_threads = n_threads_sys;
-	}
+    if (argc != 6 && argc != 7)
+    {
+        fprintf(stderr, "Usage: %s key key_mask plaintext plaintext_mask cyphertext\n", argv[0]);
+        return (0);
+    }
+    if (argc == 6)
+        n_threads = 1;
+    else
+    {
+        n_threads_sys = sysconf(_SC_NPROCESSORS_ONLN);
+        n_threads = strtol(argv[6], &end_ptr, 10);
+        if (n_threads > n_threads_sys)
+            n_threads = n_threads_sys;
+    }
 
     parse(AES_KEY_LENGTH, argv[1], key);
     n_key_mask = parse_mask(argv[2], &key_mask);
     parse(BLOCK_SIZE, argv[3], plain_text);
     n_plaintext_mask = parse_mask(argv[4], &plaintext_mask);
     parse(BLOCK_SIZE, argv[5], cypher_text);
+    search(n_key_mask, key_mask, n_plaintext_mask, plaintext_mask, key, plain_text, cypher_text);
     printf("Key: ");
     print_hex(key, AES_KEY_LENGTH);
     printf("Plain text: ");
@@ -133,12 +168,4 @@ int main(int argc, char *argv[])
     printf("Plaintext mask length: %ld\n", n_plaintext_mask);
 
     search(n_key_mask, key_mask, n_plaintext_mask, plaintext_mask, key, plain_text, cypher_text);
-	
 }
-
-
-
-
-
-
-
